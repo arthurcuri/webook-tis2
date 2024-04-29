afb4ae3e297bbb000b6d5a7b6b237d4f81b4e1c5
Hugo Macedo <hm.carvalho2002@gmail.com>
2024-04-29 19:37:48 -0300

    Back END

---
+++ src/src/main/java/com/tis2/controllers/LivroController.java
@@ -0,0 +1,62 @@
+package com.tis2.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.tis2.models.Livro;
+import com.tis2.services.LivroServices;
+
+import jakarta.validation.Valid;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/livro")
+@Validated
+public class LivroController {
+
+    @Autowired
+    private LivroServices livroServices;
+
+
+    @GetMapping("/{Id}")
+    public ResponseEntity<Livro> Id(@PathVariable Long Id){
+        Livro obj = this.livroServices.buscarPeloId(Id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> create(@Valid @RequestBody Livro obj){
+        this.livroServices.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{Id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping ("/{Id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody Livro obj, @PathVariable Long Id){
+        obj.setId(Id);  
+        this.livroServices.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{Id}")
+    public ResponseEntity<Void> delete(@PathVariable Long Id){
+        this.livroServices.delete(Id);
+        return ResponseEntity.noContent().build();
+    }
+    
+}
---
+++ src/src/main/java/com/tis2/models/Livro.java
@@ -0,0 +1,114 @@
+package com.tis2.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+
+
+
+@Entity
+@Table(name = Livro.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Livro {
+    public interface CriarLivro {}
+    public interface AtualizarLivro {}
+
+    public static final String TABLE_NAME = "livro";
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  Id;
+
+    @Column(name = "ISBN", unique = true, nullable = false)
+    @NotBlank(groups = {CriarLivro.class, AtualizarLivro.class})
+    private Long  ISBN;
+
+    @Column(name = "tituloLivro", nullable = false)
+    @NotBlank(groups = {CriarLivro.class, AtualizarLivro.class})
+    private String tituloLivro;
+
+    @NotBlank(groups = {CriarLivro.class, AtualizarLivro.class})
+    @Column(name = "secao", nullable = false)
+    private String secao;
+
+    @NotBlank(groups = {CriarLivro.class, AtualizarLivro.class})
+    @Column(name = "autor", nullable = false)
+    private String autor;
+
+    @NotBlank(groups = {CriarLivro.class, AtualizarLivro.class})
+    @Column(name = "editora", nullable = false)
+    private String editora;
+
+    @NotBlank(groups = {CriarLivro.class, AtualizarLivro.class})
+    @Column(name = "preco", nullable = false)
+    private float preco;
+   
+
+    public Long getId() {
+        return this.Id;
+    }
+
+    public void setId(Long Id) {
+        this.Id = Id;
+    }
+    
+    public Long getISBN() {
+        return this.ISBN;
+    }
+
+    public void setISBN(Long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTituloLivro() {
+        return this.tituloLivro;
+    }
+
+    public void setTituloLivro(String tituloLivro) {
+        this.tituloLivro = tituloLivro;
+    }
+
+    public String getSecao() {
+        return this.secao;
+    }
+
+    public void setSecao(String secao) {
+        this.secao = secao;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return this.editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public float getPreco() {
+        return this.preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+}
