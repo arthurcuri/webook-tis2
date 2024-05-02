2a24764463ebb0679217a8e4f7dfd3257959c705
EduardoPalharesRealePereira <dudureale04@gmail.com>
2024-05-02 18:42:09 -0300

    fixing

---
+++ src/front/gerenciamentodelivros/Iniciargerenciamento.js
@@ -0,0 +1,13 @@
+/*
+alert ('Seja bem vindo à tela de GerenciamentoWebook')
+var teste = -8 //int
+var teste2 = -20.15 // float e double
+var teste3 ="Eduardo" //string
+var teste4='false' //boolean
+document.write(teste)
+console.log(teste3) 
+document.write('<h1>Ola ' + teste3 + ' tudo bem?<h1/>')
+var nome = prompt('Digite o nome do bibliotecario: ')
+*/
+
+
